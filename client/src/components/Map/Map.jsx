0a27818b94a857512ce48358b3138b33faaa3b2f
Nic <nic.dan.costea@gmail.com>
2023-04-09 12:37:08 +0200

    view the map

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,6 +1,95 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@mui/material";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import Rating from "@mui/material/Rating";
+// import mapStyles from "../../mapStyles";
+import { MapContainer, markerContainer, paper, pointer } from "./styles";
 
 const Map = () => {
-  return <h1>Map</h1>;
+  const matches = useMediaQuery("(min-width:600px)");
+  const coordinates = { lat: 0, lng: 0 };
+  return (
+    <MapContainer>
+      <GoogleMapReact
+        // bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        bootstrapURLKeys={{ key: "AIzaSyCtvoQjuT-dfr5-0yYGZyg_YSaW5OC6t-0" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </MapContainer>
+  );
 };
+
 export default Map;
+
+// import React from "react";
+// import GoogleMapReact from "google-map-react";
+// import { Paper, Typography, useMediaQuery } from "@mui/material";
+// import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+// import Rating from "@mui/material/Rating";
+// // import mapStyles from "../../mapStyles";
+// import { mapContainer, markerContainer, paper, pointer } from "./styles";
+
+// const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherData }) => {
+//   const matches = useMediaQuery("(min-width:600px)");
+
+//   return (
+//     <div className={mapContainer}>
+//       <GoogleMapReact
+//         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+//         defaultCenter={coords}
+//         center={coords}
+//         defaultZoom={14}
+//         margin={[50, 50, 50, 50]}
+//         // options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
+//         // options={{ disableDefaultUI: true, zoomControl: true }}
+//         options={""}
+//         onChange={(e) => {
+//           setCoords({ lat: e.center.lat, lng: e.center.lng });
+//           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+//         }}
+//         onChildClick={(child) => setChildClicked(child)}
+//       >
+//         {places?.length &&
+//           places.length &&
+//           places.map((place, i) => (
+//             <div className={markerContainer} lat={Number(place.latitude)} lng={Number(place.longitude)} key={i}>
+//               {!matches ? (
+//                 <LocationOnOutlinedIcon color="primary" fontSize="large" />
+//               ) : (
+//                 <Paper elevation={3} className={paper}>
+//                   <Typography className={paper.typography} variant="subtitle2" gutterBottom>
+//                     {" "}
+//                     {place.name}
+//                   </Typography>
+//                   <img
+//                     className={pointer}
+//                     src={
+//                       place.photo
+//                         ? place.photo.images.large.url
+//                         : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+//                     }
+//                   />
+//                   <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+//                 </Paper>
+//               )}
+//             </div>
+//           ))}
+//         {weatherData?.list?.length &&
+//           weatherData.list.map((data, i) => (
+//             <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+//               <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" />
+//             </div>
+//           ))}
+//       </GoogleMapReact>
+//     </div>
+//   );
+// };
+
+// export default Map;
---
+++ client/src/components/Map/styles.js
@@ -0,0 +1,32 @@
+import { styled } from "@mui/system";
+// import { Paper, Typography, useMediaQuery } from "@mui/material";
+
+// export const mapContainer = styled("div")({
+//   height: "85vh",
+//   width: "100%",
+// });
+
+// export const markerContainer = styled("div")({
+//   position: "absolute",
+//   transform: "translate(-50%, -50%)",
+//   zIndex: 1,
+//   "&:hover": {
+//     zIndex: 2,
+//   },
+// });
+
+// export const paper = styled(Paper)({
+//   padding: "10px",
+//   display: "flex",
+//   flexDirection: "column",
+//   justifyContent: "center",
+//   width: "100px",
+// });
+
+// export const pointer = styled("img")({
+//   cursor: "pointer",
+// });
+export const MapContainer = styled("div")({
+  height: "85vh",
+  width: "100%",
+});
